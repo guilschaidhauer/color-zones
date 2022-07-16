99367abb1c3e897d7b94c65382112be097f9dc19
guilschaidhauer <e.guilherme.castro@gmail.com>
2022-07-15 22:30:54 -0300

    Add add timezone button

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var timeDisplay = document.getElementById("time");
+let timeDisplay = document.getElementById("time");
+
+let timezoneCards = [];
 
 function refreshTime() {
-  var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
-  timeDisplay.innerHTML = dateString;
+    for (let i=0; i<timezoneCards.length; i++) {
+        let dateString = getTimeString("America/Sao_Paulo");
+        timezoneCards[i].innerHTML = dateString;
+    }
 }
 
 setInterval(refreshTime, 1000);
+
+function getTimeString(timezoneName) {
+    return new Date().toLocaleString("en-US", {timeZone: timezoneName});
+}
+
+function addTimezoneCard() {
+    const paragraph = document.createElement("p");
+    const node = document.createTextNode(getTimeString());
+    paragraph.appendChild(node);
+
+    document.getElementById('timezonesCardHolder').append(paragraph);
+
+    timezoneCards.push(paragraph);
+}
