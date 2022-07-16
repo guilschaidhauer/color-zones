848a4ba880c7769594e39f13ceb79aceac1907d1
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2022-07-16 13:43:09 -0300

    Add timezonesList

---
+++ timezonesList.js
@@ -0,0 +1,17 @@
+let timezonesList = [
+    'Asia/Tokyo',
+    'Asia/Shanghai',
+    'Asia/Hong_Kong',
+    'Asia/Seoul',
+
+    'Europe/Berlin',
+    'Europe/Madrid',
+    'Europe/Paris',
+    'Europe/London',
+    'Europe/Budapest',
+    'Europe/Dublin',
+    'Europe/Rome',
+
+    'America/Sao_Paulo',
+    'America/Argentina/Buenos_Aires',
+  ];
