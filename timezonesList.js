0b97031b40034e17a93c685345919b3896624358
guilschaidhauer <e.guilherme.castro@gmail.com>
2022-07-16 13:53:01 -0300

    Add predefined timezone list

--- timezonesList.js
+++ timezonesList.js
@@ -1,17 +1,29 @@
 let timezonesList = [
-    'Asia/Tokyo',
-    'Asia/Shanghai',
-    'Asia/Hong_Kong',
-    'Asia/Seoul',
+  'Asia/Tokyo',
+  'Asia/Shanghai',
+  'Asia/Hong_Kong',
+  'Asia/Seoul',
 
-    'Europe/Berlin',
-    'Europe/Madrid',
-    'Europe/Paris',
-    'Europe/London',
-    'Europe/Budapest',
-    'Europe/Dublin',
-    'Europe/Rome',
+  'Europe/Berlin',
+  'Europe/Madrid',
+  'Europe/Paris',
+  'Europe/London',
+  'Europe/Budapest',
+  'Europe/Dublin',
+  'Europe/Rome',
 
-    'America/Sao_Paulo',
-    'America/Argentina/Buenos_Aires',
-  ];
+  'America/Sao_Paulo',
+  'America/Argentina/Buenos_Aires'
+];
+
+initializeTimezoneList();
+
+function initializeTimezoneList() {
+    let timezonesSelect = document.getElementById("timezonesSelect");
+
+    for (let i=0; i<timezonesList.length; i++) {
+        let option = document.createElement("option");
+        option.text = timezonesList[i];
+        timezonesSelect.add(option);
+    }
+}
