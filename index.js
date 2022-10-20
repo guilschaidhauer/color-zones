88c65f5946f60ec29237153e47921a48971bd038
guilschaidhauer <e.guilherme.castro@gmail.com>
2022-10-19 23:35:26 -0300

    Add "remove" button animation

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ function handleOnTimezoneInfoMouseOver() {
         this.isSelected = true;
         this.previousClass = this.classList[0];
         this.className = "timezone-info-selected";
-        this.deleteButton.style.visibility = "visible";
+        this.deleteButton.className = "remove-button";
     }
 }
 
@@ -158,7 +158,7 @@ function handleOnTimezoneInfoMouseOut() {
     if (this.isSelected) {
         this.isSelected = false;
         this.className = this.previousClass;
-        this.deleteButton.style.visibility = "hidden";
+        this.deleteButton.className = "remove-button-off";
     }
 }
 
